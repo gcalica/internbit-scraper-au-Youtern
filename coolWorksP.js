1723298935063504b9056465d89987b90046cf3f
auusui <60081732+auusui@users.noreply.github.com>
2020-07-05 09:11:01 -1000

    attempt to fix canonical form

--- coolWorksP.js
+++ coolWorksP.js
@@ -1,80 +1,72 @@
 const puppeteer = require('puppeteer');
 const fs = require('fs');
 
-function removeDuplicates(skills) {
-  return [...new Set(skills)];
-}
-
-async function fetchInfo(page, selector) {
-  let result = '';
-  try {
-
-    await page.waitForSelector(selector);
-    result = await page.evaluate((select) => document.querySelector(select).textContent, selector);
-  } catch (error) {
-    console.log('Our Error: fetchInfo() failed.\n', error.message);
-    result = 'Error';
-  }
-  return result;
-}
-
 async function getLinks(page) {
-  const links = await page.evaluate(
-      () => Array.from(
-          // eslint-disable-next-line no-undef
-          document.querySelectorAll('div[class=top-meta] a'),
-          a => a.getAttribute('href'),
-      ),
-  );
+  try {
+    let links = await page.evaluate(
+        () => Array.from(
+            // eslint-disable-next-line no-undef
+            document.querySelectorAll('div[class=top-meta] a'),
+            a => a.getAttribute('href'),
+        ),
+    );
   return links;
+  } catch (errl) {
+    console.log("error with get links", errl.message);
+  }
 }
 
 async function getAllLinks(page) {
-  let next = true;
-  const elements = [];
-  while (next === true) {
-    try {
-      // eslint-disable-next-line no-await-in-loop
-      await page.waitFor(1000);
-      getLinks(page).then(links => {
-        elements.push(links);
-      });
-      await page.waitForSelector('ul[class=paging] a[rel=next]');
-      const nextPage = await page.$('ul[class=paging] a[rel=next]');
-      await nextPage.click();
-    } catch (errp) {
-      console.log(errp.message);
-      console.log(elements);
-      next = false;
-      console.log('\nReached the end of pages!');
+  try {
+    let next = true;
+    const elements = [];
+    while (next === true) {
+      try {
+        // eslint-disable-next-line no-await-in-loop
+        await page.waitFor(1000);
+        getLinks(page).then(links => {
+          elements.push(links);
+        });
+        await page.waitForSelector('ul[class=paging] a[rel=next]');
+        const nextPage = await page.$('ul[class=paging] a[rel=next]');
+        await nextPage.click();
+      } catch (errp) {
+        console.log(errp.message);
+        console.log(elements);
+        next = false;
+        console.log('\nReached the end of pages!');
+      }
     }
+    console.log(elements);
+    return elements;
+  }catch (erra) {
+    console.log("error with getting elements", erra.message);
   }
-  console.log(elements);
-  return elements;
 }
 
 async function getPageInfo(page, elements) {
-  let JobsScraped = 0;
-  const details = [];
-  for (let i = 0; i < elements.length; i++) {
-    for (let j = 0; j < elements[i].length; j++) {
-      const url = 'https://www.coolworks.com${elements[i][j]}';
+  try {
+    let JobsScraped = 0;
+    const details = [];
+    for (let i = 0; i < elements.length; i++) {
+      for (let j = 0; j < elements[i].length; j++) {
+        const url = 'https://www.coolworks.com${elements[i][j]}';
 
-      const company = list.querySelector('div.top-meta h5');
-      const location = list.querySelector('p[class=locations] a');
-      const posted = list.querySelector('div[class=link-job] span');
-      let skills = '';
-      skills = page.evaluate(
-          () => Array.from(
-              document.querySelectorAll('p[class=blurb]')),
-      );
-      if (skills.length === 0) {
-        skills = 'N/A';
-      } else {
-        skills = removeDuplicates(skills);
-      }
-      await page.goto(url);
-      await page.waitForNavigation();
+        const company = list.querySelector('div.top-meta h5');
+        const location = list.querySelector('p[class=locations] a');
+        const posted = list.querySelector('div[class=link-job] span');
+        let skills = '';
+        skills = page.evaluate(
+            () => Array.from(
+                document.querySelectorAll('p[class=blurb]')),
+        );
+        if (skills.length === 0) {
+          skills = 'N/A';
+        } else {
+          skills = removeDuplicates(skills);
+        }
+        await page.goto(url);
+        await page.waitForNavigation();
         const position = fetchInfo(page, 'div[class=ttl-decor fixed-ttl] h1')
         const contact = fetchInfo(page, 'ul[class=contact-list] li');
         const compensation = fetchInfo(page, 'div[class=benefits]');
@@ -84,7 +76,7 @@ async function getPageInfo(page, elements) {
 
         JobsScraped++;
 
-      details.push({
+        details.push({
           position: position,
           company: company,
           location: location,
@@ -96,19 +88,33 @@ async function getPageInfo(page, elements) {
           url: url,
           lastScraped: lastScraped,
         });
+      }
     }
+    console.log('Total Jobs Scraped: ', JobsScraped);
+    return details;
+  } catch (errp) {
+    console.log("error with getting page info", errp.message);
   }
-  console.log('Total Jobs Scraped: ', JobsScraped);
-  return details;
 }
 
+async function fetchInfo(page, selector) {
+  let result = '';
+  try {
 
+    await page.waitForSelector(selector);
+    result = await page.evaluate((select) => document.querySelector(select).textContent, selector);
+  } catch (error) {
+    console.log('Error with fetching info', error.message);
+    result = 'Error';
+  }
+  return result;
+}
 
 (async () => {
   try {
       let browser = await puppeteer.launch({ slowMo: 250, devtools: true }); // Slow down by 250 ms
       let page = await browser.newPage();
-
+      await page.goto('https://www.coolworks.com/');
       // search technology jobs
       await page.waitForSelector('input[id="search_keywords"]');
       // change this text for what you want to search
@@ -134,9 +140,7 @@ async function getPageInfo(page, elements) {
 
     await browser.close();
 
-  } catch (error) {
-    console.log('Something went wrong', error.message);
-    await browser.close();
-    console.log(error("Browser now closed"));
+  } catch (e) {
+    console.log('Something went wrong', e.message);
   }
 })();
